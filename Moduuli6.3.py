56d91498fb24ab8fb06b2fd4d422ec64394f37f4
eerovii <111981034+eerovii@users.noreply.github.com>
2022-09-14 13:07:03 +0300

    Update Moduuli6.3.py

--- Moduuli6.3.py
+++ Moduuli6.3.py
@@ -1,6 +1,8 @@
 def galToL(val):
     return val*3.78541178
-gal=float(input('Anna gallonien määrä: '))
+
+gal=float(input('Anna määrä galloneissa: '))
+
 while gal>0:
     print(f'{galToL(gal):6.2f} litraa')
-    gal = float(input('Anna gallonien määrä: '))
+    gal = float(input('Anna määrä galloneissa: '))
